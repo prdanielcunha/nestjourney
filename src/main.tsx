031b1f4447803dada269f4ac417e100a041636dc
prdanielcunha <pastordanielpcunha@gmail.com>
2026-09-17 19:35:13 -0300

    feat: expose Presence Assist as safe vertical route

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,20 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { EcosystemSessionGate } from './EcosystemSessionGate.tsx'
+import PresenceAssistPage from './PresenceAssistPage.tsx'
 
 if ('serviceWorker' in navigator && import.meta.env.PROD) {
   window.addEventListener('load', () => navigator.serviceWorker.register('/sw.js'))
 }
 
+const journeyExperience = window.location.pathname === '/presence-assist'
+  ? <PresenceAssistPage />
+  : <App />
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <EcosystemSessionGate>
-      <App />
+      {journeyExperience}
     </EcosystemSessionGate>
   </StrictMode>,
 )
