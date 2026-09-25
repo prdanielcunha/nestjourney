9dce1aa5c3e9e99bf055c975cc4c570ee697257a
prdanielcunha <pastordanielpcunha@gmail.com>
2026-09-25 00:19:42 -0300

    brand: point design contract to canonical raster assets

--- src/design-system/digitalPremium.ts
+++ src/design-system/digitalPremium.ts
@@ -11,10 +11,10 @@ export const DIGITAL_PREMIUM = {
   },
   assets: {
     favicon: '/favicon.ico',
-    faviconSvg: '/favicon.svg',
     appleTouchIcon: '/apple-touch-icon.png',
     fullBrand: '/brand/nestjourney-horizontal-light.png',
-    symbol: '/brand/nestjourney-symbol.svg',
+    symbol: '/brand/nestjourney-symbol-light.png',
+    socialImage: '/brand/nestjourney-og.png',
   },
   minTouchTargetPx: 44,
 } as const
